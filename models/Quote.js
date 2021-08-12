eb8a6feb881e224caf9c76e6e0a1151172001884
JJLindsey <jlindsey010@gmail.com>
2021-08-12 11:32:17 -0700

    Quote Schema

---
+++ models/Quote.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const QuoteSchema = new Schema({
+    quoteText: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+    }
+);
+
+QuoteSchema.virtual('reactionCount').get(function () {
+    return reactions.length;
+});
+
+const Quote = model('Quote', QuoteSchema);
+
+module.exports = Quote
